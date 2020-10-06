9a9a46670fed2927ebf251031507f942e5367313
Melanie Tran <melaniexx02@gmail.com>
2020-10-06 10:10:57 -0600

    Setup Search Controller

--- spec/features/user_can_visit_root_path_spec.rb
+++ spec/features/user_can_visit_root_path_spec.rb
@@ -8,4 +8,20 @@ describe "User can visit root path" do
     expect(page).to have_content('Welcome to Poetry and Emotion')
     expect(page).to have_selector('input')
   end
+
+  it "can search poems by author and display the first 10 poems" do
+    visit '/'
+    fill_in 'author', with: 'Emily'
+    click_on 'Get Poems'
+
+    expect(current_path).to eq("/search")
+  end
 end
+
+
+# As a user
+# When I visit "/"
+# And I fill in "Emily" in the textfield(Note: Use the existing search form)
+# And I click "Get Poems"
+# Then I should be on page "/search"
+# Then I should see a list of the first 10 poems.
