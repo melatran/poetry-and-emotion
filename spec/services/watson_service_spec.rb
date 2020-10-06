6b1069d415c8adc0dad8610b364eca645ba1a9e3
Melanie Tran <melaniexx02@gmail.com>
2020-10-06 15:01:43 -0600

    Update test with poem object

--- spec/services/watson_service_spec.rb
+++ spec/services/watson_service_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe WatsonService do
   it ".analyze_poem" do
-    lines = "Not at Home to Callers, Says the Naked Tree, Bonnet due in April --, Wishing you Good Day --"
+    poem_lines = "Not at Home to Callers, Says the Naked Tree, Bonnet due in April --, Wishing you Good Day --"
+    poem = Poem.new(author: 'Emily Dickinson', title: 'Not at Home to Callers', lines: poem_lines, linecount:'4')
     service = WatsonService.new
-    tone_analyzer = service.analyze_poem(lines)
+    tone_analyzer = service.analyze_poem(poem.lines)
     
     expect(tone_analyzer).to have_key(:document_tone)
     expect(tone_analyzer[:document_tone]).to have_key(:tones)
